076db99dc7fefa4c66ea4caf7c9b26ef5c1c1d35
Sly Cooper <slycooper50@hotmail.com>
2019-12-21 23:21:14 +0300

    Update

---
+++ PythonSetup/ahmedpkg/__init__.py
@@ -0,0 +1,3 @@
+class ahmed:
+    def __init__():
+        self.value="Cannot be quantified"
--- PythonSetup/setup.py
+++ PythonSetup/setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
-with open("README.md, "r"") as fh:
+with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-name="AhmedPackage",
+name="ahmedpkg",
 version="1.00",
 author="Ahmed Sadiq",
 description="My first python package",
 long_description="This is my firts python package.",
 long_description_content_type="text/markdown",
 packages=setuptools.find_packages(),
-classifiers=["python package"],
+classifiers=["Programming Language :: Python :: 3"],
 python_requires='>=3.0',)
